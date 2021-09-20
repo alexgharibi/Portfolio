b7a4b7231a39bc1dae869fb57f61184aa64f90c9
Alex <ali52346@gmail.com>
2021-09-20 09:29:48 -0400

    updating business module

---
+++ src/pages/BusinessExpenses/ExpensesItem/ExpensesItem.js
@@ -0,0 +1,18 @@
+import ExpensesDate from "./ExpensesDate";
+import classes from "./ExpensesItem.module.css";
+
+const ExpensesItem = (props) => {
+  return (
+    <li>
+      <div className={classes["expense-item"]}>
+        <ExpensesDate date={props.date} />
+        <div className={classes["expense-item__description"]}>
+          <h2 layout>{props.title}</h2>
+          <div className={classes["expense-item__price"]}>${props.amount}</div>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default ExpensesItem;
