84d91aeb484808a29e21013a86ba409783d6d899
Alex <ali52346@gmail.com>
2021-09-27 18:33:47 -0400

    Rest API

--- src/pages/React-Hooks/components/Ingredients/Ingredients.js
+++ src/pages/React-Hooks/components/Ingredients/Ingredients.js
@@ -1,19 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import IngredientForm from "./IngredientForm";
+import Search from "./Search";
+import IngredientList from "./IngredientList";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+const Ingredients = () => {
+  const [ingre, setIngre] = useState([]);
 
-function Ingredients() {
+  useEffect(() => {
+    fetch(
+      "https://react-hooks-19787-default-rtdb.firebaseio.com/ingredients.json"
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngre = [];
+        for (const key in responseData) {
+          loadedIngre.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setIngre(loadedIngre);
+      });
+  }, []);
+
+  const addIngreHandler = (ingred) => {
+    fetch(
+      "https://react-hooks-19787-default-rtdb.firebaseio.com/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingred),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setIngre((prevIngred) => [
+          ...prevIngred,
+          { id: responseData.name, ...ingred },
+        ]);
+      });
+  };
+
+  const removeIngreHandler = (ingreId) => {
+    setIngre((prevIngred) =>
+      prevIngred.filter((ingre) => ingre.id !== ingreId)
+    );
+  };
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngred={addIngreHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={ingre} onRemoveItem={removeIngreHandler} />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
