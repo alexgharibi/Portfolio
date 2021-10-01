ac80102e928b71aba032ecd5be0010be9c6b44a1
Alex <ali52346@gmail.com>
2021-10-01 11:13:03 -0400

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from "react";
-import ExpensesMainPage from "./pages/BusinessExpenses/ExpensesMainPage";
-import MainAnimationPage from "./pages/Animations/MainAnimationPage";
-import FavoritesMainPage from "./pages/Favorites/FavoritesMainPage";
 import MainPage from "./pages/MainPage/MainPage";
 import { Route, Redirect, Switch } from "react-router-dom";
 import About from "./pages/About/About";
-import TopPage from "./pages/MainPage/TopPage";
 import F1 from "./pages/F1/F1";
 import ReactHooks from "./pages/React-Hooks/ReactHooks";
 
@@ -14,9 +10,7 @@ const App = () => {
     <React.Fragment>
       <main>
         <Switch>
-          <Route path="/" exact>
-            <MainPage />
-          </Route>
+          <Route path="/" exact component={MainPage} />
           <Route path="/about" exact component={About} />
           <Route path="/f1" exact component={F1} />
           <Route path="/ingrediants" exact component={ReactHooks} />
--- src/pages/React-Hooks/ReactHooks.js
+++ src/pages/React-Hooks/ReactHooks.js
@@ -1,20 +1,10 @@
-import Ingredients from "./components/Ingredients/Ingredients";
 import AuthContextProvider from "./context/auth-context";
-import Auth from "./components/Auth";
-import { useContext } from "react";
-import { AuthContext } from "./context/auth-context";
+import MainPage from "./MainPage";
 
 const ReactHooks = () => {
-  const authContext = useContext(AuthContext);
-
-  let content = <Auth />;
-  if (authContext.isAuth) {
-    content = <Ingredients />;
-  }
-
   return (
     <AuthContextProvider>
-      <div>{content}</div>
+      <MainPage />
     </AuthContextProvider>
   );
 };
