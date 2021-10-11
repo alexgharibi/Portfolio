e4753a684088f0142b1e3db936663965917305bf
alexgharibi <alex.morin.gh@gmail.com>
2021-10-11 10:41:01 -0400

    error portal

--- src/Alex.js
+++
@@ -1,3 +0,0 @@
-const alex = () => {};
-
-export default alex;
--- src/pages/React-Hooks/components/UI/ErrorModal.js
+++ src/pages/React-Hooks/components/UI/ErrorModal.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./ErrorModal.css";
 
-import './ErrorModal.css';
+const Backdrop = (props) => {
+  return <div className="backdrop" onClick={props.onClose} />;
+};
 
-const ErrorModal = React.memo(props => {
+const Modaloverlay = (props) => {
+  return (
+    <div className="error-modal">
+      <h2>An Error Occurred!</h2>
+      <p>{props.children}</p>
+      <div className="error-modal__actions">
+        <button type="button" onClick={props.onClose}>
+          Okay
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const ErrorModal = React.memo((props) => {
   return (
     <React.Fragment>
-      <div className="backdrop" onClick={props.onClose} />
-      <div className="error-modal">
-        <h2>An Error Occurred!</h2>
-        <p>{props.children}</p>
-        <div className="error-modal__actions">
-          <button type="button" onClick={props.onClose}>
-            Okay
-          </button>
-        </div>
-      </div>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <Modaloverlay onClose={props.onClose} children={props.children} />,
+        document.getElementById("modal-root")
+      )}
     </React.Fragment>
   );
 });
